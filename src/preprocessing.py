f61127310dd62d809337d6f95b416ee1ac87b6d5
oliviazura <oszura@student.ysu.edu>
2026-04-12 14:08:03 -0400

    Fixed issue with index appearing incorrectly with data. Added conversion to American GPA system for ease of understanding.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,14 +2,20 @@ import numpy as np
 import pandas as pd
 
 #import data from kaggle
-data = pd.read_csv("student_depression_dataset.csv")
+data = pd.read_csv("data/student_depression_dataset.csv")
+data = data.reset_index(drop = True)
 
 #focus on undergraduate students that are students from ages 18-22 (traditional 4-year program)
 data = data[data['Age'] <= 22]
 data = data[data['Degree'].str.startswith('B')]
 
 #drop unneccessary columns
-data = data.drop(columns = ["id", "City", "Work Pressure", "Profession"])
+data = data.drop(columns = ["id","City","Job Satisfaction", "Work Pressure", "Profession"])
+
+#convert 10 point cgpa to 4 point gpa
+data["GPA"] = data["CGPA"] / 10 * 4
+data["GPA"] = data["GPA"].round(2)
+data = data.drop(columns= ["CGPA"])
 
 #save processed data
-data.to_csv('processed_data.csv')
+data.to_csv('data/processed_data.csv', index=False)
