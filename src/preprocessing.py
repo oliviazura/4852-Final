840815452e9fbce61eba1a8793ae98edd3e89d91
oliviazura <oszura@student.ysu.edu>
2026-04-12 15:25:41 -0400

    added train/test split to preprocessing file

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+
 #import data from kaggle
 data = pd.read_csv("data/student_depression_dataset.csv")
 data = data.reset_index(drop = True)
@@ -18,3 +20,8 @@ data = data.drop(columns = ["id","City","Job Satisfaction", "Work Pressure", "Pr
 
 #save processed data
 data.to_csv('data/processed_data.csv', index=False)
+
+train, test = train_test_split(data, test_size=0.3, random_state=0)
+
+train.to_csv('data/train_data.csv', index = False)
+test.to_csv('data/test_data.csv', index = False)
